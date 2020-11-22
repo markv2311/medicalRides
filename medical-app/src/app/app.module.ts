a98c0bb8686de63baefb729071c671f756dac1f7
Tim Keenan <tkeenan911@gmail.com>
2020-11-22 13:47:23 -0500

    Adds two new components for the dispatch page.

--- medical-app/src/app/app.module.ts
+++ medical-app/src/app/app.module.ts
@@ -9,13 +9,17 @@ import {MatButtonModule} from '@angular/material/button';
 import { DispatchMainPageComponent } from './views/dispatch-main-page/dispatch-main-page.component';
 import { BottomNavComponent } from './widgets/bottom-nav/bottom-nav.component';
 import { MatIconModule } from '@angular/material/icon';
+import { DispatchJobQueueComponent } from './views/dispatch-main-page/dispatch-job-queue/dispatch-job-queue.component';
+import { DispatchActiveDriversComponent } from './views/dispatch-main-page/dispatch-active-drivers/dispatch-active-drivers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TestComponentComponent,
     DispatchMainPageComponent,
-    BottomNavComponent
+    BottomNavComponent,
+    DispatchJobQueueComponent,
+    DispatchActiveDriversComponent
   ],
   imports: [
     BrowserModule,
---
+++ medical-app/src/app/views/dispatch-main-page/dispatch-active-drivers/dispatch-active-drivers.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'dispatch-active-drivers',
+  templateUrl: './dispatch-active-drivers.component.html',
+  styleUrls: ['./dispatch-active-drivers.component.less']
+})
+export class DispatchActiveDriversComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
