741e589b1a599b262385b02d9b1b42640c67be20
Chipper <calumon0@gmail.com>
2020-11-23 16:50:35 -0500

    commit

--- medical-app/src/app/app-routing.module.ts
+++ medical-app/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {DispatchMainPageComponent} from './views/dispatch-main-page/dispatch-main-page.component';
 import {DriverMainPageComponent} from './views/driver-main-page/driver-main-page.component';
-
+//^this here is how to route to certain pages/files on the actual page
 const routes: Routes = [
   { path: '', redirectTo: 'dispatch', pathMatch: 'full' },
   { path: 'dispatch', component: DispatchMainPageComponent},
@@ -15,3 +15,4 @@ const routes: Routes = [
 })
 export class AppRoutingModule { }
 export const routingComponents = [DispatchMainPageComponent, DriverMainPageComponent];
+//^the parameters are the pages
