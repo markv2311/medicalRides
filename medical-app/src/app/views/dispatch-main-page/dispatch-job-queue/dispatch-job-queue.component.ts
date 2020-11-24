575b6ad0b941e2cd8401874565259ef3a994afa9
Chipper <calumon0@gmail.com>
2020-11-24 16:45:31 -0500

    commit

--- medical-app/src/app/views/dispatch-main-page/dispatch-job-queue/dispatch-job-queue.component.ts
+++ medical-app/src/app/views/dispatch-main-page/dispatch-job-queue/dispatch-job-queue.component.ts
@@ -7,15 +7,23 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class DispatchJobQueueComponent implements OnInit {
   status: boolean = true;
+  status2: boolean = false;
+  status3: boolean = true;
+  //all buttons must have diffrent status values.
+
   pickup: string ='1242 Thing Ave';
   destination: string ='64 Stone Rd';
   driver: string ='Needs Assignment';
+
   pickup2: string ='295 Robin Rd';
   destination2: string ='125 University Ave';
   driver2: string ='Rolling';
+
   pickup3: string ='249 Yellow Rock Dr';
   destination3: string ='385 W.Main St';
   driver3: string ='Clear';
+//This is where the data for the buttons will be taken and given to the html file.
+
  // @Input() pickup: 'pikachu'|'keep going';
   constructor() { }
   ngOnInit(): void {
