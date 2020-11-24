5ad2e490c7e78806fad55aab60fdca8c7e6bf147
Tim Keenan <tkeenan911@gmail.com>
2020-11-24 18:22:34 -0500

    Adds a job toggle switch and adds additional job information

--- medical-app/src/app/services/job-selector.service.ts
+++ medical-app/src/app/services/job-selector.service.ts
@@ -18,12 +18,22 @@ export class JobSelectorService {
     this.jobOne.pickupAddress = '123 Generic Lane';
     this.jobOne.pickupTime = '2:30 PM';
     this.jobOne.dropOffTime = '3:30 PM';
+    this.jobOne.dropOffDetails = 'Main Entrance';
+    this.jobOne.tripDistance = 5.7;
+    this.jobOne.numPassengers = 1;
+    this.jobOne.vehicleType = 'Double';
+    this.jobOne.notes = 'The pickup location is farther than normal.  Arrive earlier than normal to account for traffic.';
     /** TECHNIQUE 2: Use JSON (preferred approach) */
     this.jobTwo = {
       destinationAddress: 'Rochester Regional Hospital',
       pickupAddress: '43 Rainbow Road',
       pickupTime: '12:00 PM',
-      dropOffTime: '1:00 PM'
+      dropOffTime: '1:00 PM',
+      dropOffDetails: 'Back Service Gate',
+      tripDistance: 5.5,
+      numPassengers: 0,
+      vehicleType: 'Single',
+      notes: 'Passenger tends to run late.  Arrive extra early.'
     };
     // By default, the current job will initialize to jobOne
     this.currentJob = this.jobOne;
