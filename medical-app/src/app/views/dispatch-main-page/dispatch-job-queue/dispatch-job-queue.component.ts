46ea8ed28c069c3a118e82b70332cc4b01c08eba
Chipper <calumon0@gmail.com>
2020-11-24 03:32:23 -0500

    commit

--- medical-app/src/app/views/dispatch-main-page/dispatch-job-queue/dispatch-job-queue.component.ts
+++ medical-app/src/app/views/dispatch-main-page/dispatch-job-queue/dispatch-job-queue.component.ts
@@ -4,6 +4,7 @@ import { Component, OnInit } from '@angular/core';
   selector: 'dispatch-job-queue',
   templateUrl: './dispatch-job-queue.component.html',
   styleUrls: ['./dispatch-job-queue.component.less']
+  <button>{{ pikachu }}</button>
 })
 export class DispatchJobQueueComponent implements OnInit {
 
