71bffc895e0bce3ff8842ec38cda886decfbad6e
Tim Keenan <tkeenan911@gmail.com>
2020-11-24 14:41:51 -0500

    Adds data class with sample data using JSON and OOP techniques

--- medical-app/src/app/app.module.ts
+++ medical-app/src/app/app.module.ts
@@ -4,20 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { TestComponentComponent } from './components/test-component/test-component.component';
 import {MatButtonModule} from '@angular/material/button';
 import { DispatchMainPageComponent } from './views/dispatch-main-page/dispatch-main-page.component';
 import { BottomNavComponent } from './widgets/bottom-nav/bottom-nav.component';
 import { MatIconModule } from '@angular/material/icon';
-import {DispatchActiveDriversComponent} from "./views/dispatch-main-page/dispatch-active-drivers/dispatch-active-drivers.component";
+import {DispatchActiveDriversComponent} from './views/dispatch-main-page/dispatch-active-drivers/dispatch-active-drivers.component';
 import { DriverMainPageComponent } from './views/driver-main-page/driver-main-page.component';
-import {DispatchJobQueueComponent} from "./views/dispatch-main-page/dispatch-job-queue/dispatch-job-queue.component";
+import {DispatchJobQueueComponent} from './views/dispatch-main-page/dispatch-job-queue/dispatch-job-queue.component';
 import { DriverDetailsPageComponent } from './views/driver-main-page/driver-details-page/driver-details-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponentComponent,
     DispatchMainPageComponent,
     BottomNavComponent,
     routingComponents,
--- medical-app/src/app/services/job-selector.service.ts
+++ medical-app/src/app/services/job-selector.service.ts
@@ -1,16 +1,35 @@
 import { Injectable } from '@angular/core';
+import {Job} from '../classes/job';
 
 @Injectable({
   providedIn: 'root'
 })
 export class JobSelectorService {
 
-  constructor() { }
+  // Adds three sample jobs that can be used within the application
+  jobOne: Job;
+  jobTwo: Job;
+
+  constructor() {
+    /** TECHNIQUE 1: Create object using OOP techniques */
+    this.jobOne = new Job();
+    this.jobOne.destinationAddress = 'Unity Hospital';
+    this.jobOne.pickupAddress = '123 Generic Lane';
+    this.jobOne.pickupTime = '2:30 PM';
+    this.jobOne.dropOffTime = '3:30 PM';
+    /** TECHNIQUE 2: Use JSON */
+    this.jobTwo = {
+      destinationAddress: 'Rochester Regional Hospital',
+      pickupAddress: '43 Rainbow Road',
+      pickupTime: '12:00 PM',
+      dropOffTime: '1:00 PM'
+    };
+  }
 
   /**
    *
    */
-  getCurrentJob(): void {
+  getCurrentJob(): Job {
 
   }
 }
