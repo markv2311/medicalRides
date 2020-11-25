a5e1e6f7b364008407e82e83e281aab0603ba074
Chipper <calumon0@gmail.com>
2020-11-25 01:42:37 -0500

    commit

--- medical-app/src/app/app-routing.module.ts
+++ medical-app/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {DispatchMainPageComponent} from './views/dispatch-main-page/dispatch-main-page.component';
+import {DispatcherOptionsComponent} from './views/dispatch-main-page/dispatcher-options/dispatcher-options.component';
 import {DriverMainPageComponent} from './views/driver-main-page/driver-main-page.component';
 import {DriverDetailsPageComponent} from './views/driver-main-page/driver-details-page/driver-details-page.component';
 
