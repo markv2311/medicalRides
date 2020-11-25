e1f88b27fd27dbaa1dcfb45fcf24d5a3116c7f31
Chipper <calumon0@gmail.com>
2020-11-25 01:11:57 -0500

    commit

--- medical-app/src/app/app-routing.module.ts
+++ medical-app/src/app/app-routing.module.ts
@@ -16,4 +16,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponents = [DispatchMainPageComponent, DriverMainPageComponent, DriverDetailsPageComponent];
+export const routingComponents = [DispatchMainPageComponent, DriverMainPageComponent, DriverDetailsPageComponent, DispatcherOptionsComponent];
---
+++ medical-app/src/app/views/dispatch-main-page/dispatcher-options/dispatcher-options.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dispatcher-options',
+  templateUrl: './dispatcher-options.component.html',
+  styleUrls: ['./dispatcher-options.component.less']
+})
+export class DispatcherOptionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
