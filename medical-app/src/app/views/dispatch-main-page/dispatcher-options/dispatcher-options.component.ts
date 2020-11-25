15747d4a819e49342f9f0eb14f25f0509fb8319c
Chipper <calumon0@gmail.com>
2020-11-25 02:41:22 -0500

    commit

--- medical-app/src/app/views/dispatch-main-page/dispatcher-options/dispatcher-options.component.ts
+++ medical-app/src/app/views/dispatch-main-page/dispatcher-options/dispatcher-options.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-dispatcher-options',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DispatcherOptionsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  navigateToDispatchMainPageComponent(): void{
+    this.router.navigate(['./dispatch-main-page/dispatch-main-page.component']);
   }
 
 }
